910572e062750916d2e9d12e3f79a0dfeda5dd65
Teko <tekofxx@gmail.com>
2026-06-21 23:00:56 +0200

    Updated wxt config

--- wxt.config.ts
+++ wxt.config.ts
@@ -4,7 +4,19 @@ import { defineConfig } from "wxt";
 export default defineConfig({
   modules: ["@wxt-dev/module-react"],
   srcDir: "src",
+
   manifest: {
-    permissions: ["dataCollection"],
+    name: "Wooacry Extension",
+    version: "1.0.0",
+    description: "Extensión for splitting payment between people",
+    homepage_url: "https://github.com/tekofx/wooacry-extension",
+    browser_specific_settings: {
+      gecko: {
+        id: "wooacry-extension@tekofx",
+        data_collection_permissions: {
+          required: [],
+        },
+      },
+    },
   },
 });
