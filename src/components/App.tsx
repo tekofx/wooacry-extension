73d54d87fe79570c6b42a691472c51e69eb187f3
Teko <tekofxx@gmail.com>
2026-06-21 21:49:57 +0200

    Removed button to get data

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -16,27 +16,29 @@ function App() {
   const [opened, { toggle }] = useDisclosure();
   const [data, setData] = useState<MessageResponse>();
 
-  async function getPageSource() {
-    const [tab] = await browser.tabs.query({
-      active: true,
-      currentWindow: true,
-    });
-    if (!tab.id) return;
+  useEffect(() => {
+    const getPageSource = async () => {
+      const [tab] = await browser.tabs.query({
+        active: true,
+        currentWindow: true,
+      });
+      if (!tab.id) return;
 
-    try {
-      const response = (await browser.tabs.sendMessage(tab.id, {
-        type: "EXTRACT_CART_DATA",
-      })) as MessageResponse;
-      setData(response);
-    } catch (error) {
-      console.error("Failed to get HTML:", error);
-    }
-  }
+      try {
+        const response = (await browser.tabs.sendMessage(tab.id, {
+          type: "EXTRACT_CART_DATA",
+        })) as MessageResponse;
+        setData(response);
+      } catch (error) {
+        console.error("Failed to get HTML:", error);
+      }
+    };
+    getPageSource();
+  }, []);
+
   return (
     <AppShell padding="md">
       <AppShell.Main>
-        <Button onClick={() => getPageSource()}>Clicl</Button>
-
         {data !== undefined && (
           <Stack>
             <MoneyField text="Subtotal" quantity={data.subtotal} />
